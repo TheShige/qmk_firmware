968335a1255ab2b8ef52d80753d8f89a04773996
unknown <gvidas.sutkus@ktu.edu>
2023-12-19 23:38:35 +0200

    Added a keyboard config for via

---
+++ keyboards/shige/lotus58/keymaps/via/rules.mk
@@ -0,0 +1,11 @@
+# Enabling via
+VIA_ENABLE=yes
+
+# Features
+ENCODER_MAP_ENABLE = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+# Achordion mod
+SRC += features/achordion.c
