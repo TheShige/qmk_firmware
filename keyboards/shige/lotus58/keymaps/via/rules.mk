aa42b68a66af40e0b32fd762728dbb707f098a0f
TheShige <sutkugvidas@gmail.com>
2024-02-26 04:35:57 +0200

    Moved debouncing lines from info.json to config.h, changed the debouncing algorithm, reduced the tapping term

--- keyboards/shige/lotus58/keymaps/via/rules.mk
+++ keyboards/shige/lotus58/keymaps/via/rules.mk
@@ -1,11 +1,14 @@
 # Enabling via
-VIA_ENABLE=yes
+VIA_ENABLE = yes
 
 # Features
 ENCODER_MAP_ENABLE = yes
 OLED_ENABLE = yes
 WPM_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
+
+# Debouncing
+DEBOUNCE_TYPE = asym_eager_defer_pk
 
 # Achordion mod
 SRC += features/achordion.c
